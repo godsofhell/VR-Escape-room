5a968cfd6af0fbfda1522df48070c108d6d0b0c4
godsofhell <viplavpatel2004@gmail.com>
2024-11-17 01:07:01 +0530

    lift

--- Assets/scripts/PasswordCheck.cs
+++ Assets/scripts/PasswordCheck.cs
@@ -1,28 +1,57 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PasswordCheck : MonoBehaviour
 {
     public TMP_InputField passwordInputField; // Reference to the TMP InputField
     public string correctPassword = "IRIS"; // The correct password
-    public TextMeshProUGUI resultText; // Reference to a TMP Text for displaying results
+    // to asssign the VR button in Inspector
+    public Button enterButton;
+   
+    public GameObject lift;
+    public GameObject tile;
 
+    public Collider LiftCollider;
     // Method to check if the password is correct
-    public void CheckPassword()
+     void Start()
     {
-        // Get the text entered in the TMP_InputField
-        string enteredPassword = passwordInputField.text;
+        
+        tile.SetActive(false);
+        lift.SetActive(false);
 
-        // Check if the entered password matches the correct password
-        if (enteredPassword == correctPassword)
+        if(enterButton != null )
         {
-            resultText.text = "Password is correct!";
-            resultText.color = Color.green; // Optional: Change the color of the result text
+            enterButton.onClick.AddListener(CheckPassword);
         }
         else
         {
-            resultText.text = "Incorrect password. Try again.";
-            resultText.color = Color.red; // Optional: Change the color of the result text
+            Debug.Log("Enter Button is not assigned");
+        }
+
+    }
+    public void CheckPassword()
+    {
+        if (passwordInputField != null)
+        {
+
+
+            // Get the text entered in the TMP_InputField
+            string enteredPassword = passwordInputField.text;
+
+            // Check if the entered password matches the correct password
+            if (enteredPassword == correctPassword)
+            {
+                LiftCollider.isTrigger = true;
+                lift.SetActive(true);
+                tile.SetActive(true);
+
+                Debug.Log("password is correct"); // Optional: Change the color of the result text
+            }
+        }
+        else
+        {
+            Debug.Log("password is incorrect"); // Optional: Change the color of the result text
         }
     }
 }
